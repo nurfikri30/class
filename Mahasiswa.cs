84d3f32f90e95d6ee2b6d357292a53286a9cb3c0
fikri <muhammad.2910@students.amikom.ac.id>
2019-03-19 12:15:51 +0700

    bikin class

---
+++ Mahasiswa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app2
+{
+    public class Mahasiswa
+    {
+        public string Nim { get; set; }
+        public string Nama { get; set; }
+        public float Ipk { get; set; }
+
+        public void Registrasi()
+        {
+            Console.WriteLine("Nama : {0}", Nama);
+            Console.WriteLine("NIM : {0}", Nim);
+            Console.WriteLine("IPK : {0}", Ipk);
+            Console.WriteLine("Telah melakukan registrasi");
+        }
+        public void Isikrs()
+        {
+            Console.WriteLine("{0} Sedang malakukan proses krs\n", Nama);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app1
+{
+    public class Program
+
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            saya.Nim = "18.11.2338";
+            saya.Nama = "Fikri";
+            saya.Ipk = 40f;
+
+            kamu.Nim = "18.11.2338";
+            kamu.Nama = "Fikri";
+            kamu.Ipk = 40f;
+
+            saya.Registrasi();
+            saya.Isikrs();
+
+            kamu.Registrasi();
+            kamu.Isikrs();
+
+            Console.ReadKey();
+        }
+    }
+}
